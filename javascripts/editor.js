f62e429d1bb522a76f1b386011a512663da1a596
royjang <zzl11081@gmai.com>
2017-05-25 15:30:36 +0800

    Update 2017-05-25T07:30:34.152Z

--- javascripts/editor.js
+++ javascripts/editor.js
@@ -17,16 +17,6 @@ function init() {
     controls.dynamicDampingFactor = 0.3;
     controls.addEventListener( 'change', render );
 
-    // Ground
-    var plane = new THREE.Mesh(
-        new THREE.PlaneBufferGeometry( 300, 300 ),
-        new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
-    );
-    plane.rotation.x = -Math.PI/2;
-    plane.position.y = -0.5;
-    scene.add( plane );
-    plane.receiveShadow = true;
-
     scene.add( new THREE.HemisphereLight( 0x443333, 0x111122 ) );
     addShadowedLight( 1, 1, 1, 0xffffff, 1.35 );
     addShadowedLight( 0.5, 1, -1, 0xffaa00, 1 );
